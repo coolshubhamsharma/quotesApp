770bde79103143a481508b1cc818c0138d312826
coolshubhamsharma <coolshubhamsharma26@gmail.com>
2024-09-06 11:12:24 +0530

    codes

--- front-end/src/components/Pages/AllQuotes.js
+++ front-end/src/components/Pages/AllQuotes.js
@@ -7,7 +7,7 @@ const AllQuotes = () => {
     let [quotes , setQuotes] = useState([]); //when we change the value of a variable in the code it is not reflected upon the server(ie. value change hoti hai par aapka component render nhi hota) so we have to use useState to set the value of the variable
 
     async function getQuotes(){
-        let res = await axios.get('http://localhost:8080/allquotes');
+        let res = await axios.get(`https://quotesapp-j4ap.onrender.com/allquotes`);
         setQuotes(res.data);
     }
 
